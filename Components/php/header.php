09e4b40ac73a8ba6f9a13ecd905791a4f6a24c49
Atzahel <armandoatzahell@gmail.com>
2023-04-04 13:22:03 -0600

    Se agregó el header para la página para desktop con las configuraciones correspondientes a la empresa Consorcio Internacional Caporali

---
+++ Components/php/header.php
@@ -0,0 +1,63 @@
+<header>
+  <nav>  
+    <svg id="Grupo_3" data-name="Grupo 3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="101.663" height="72.985" viewBox="0 0 101.663 72.985">
+      <defs>
+        <clipPath id="clip-path">
+          <rect id="Rectángulo_2" data-name="Rectángulo 2" width="101.663" height="72.985" fill="none"/>
+        </clipPath>
+      </defs>
+      <path id="Trazado_1" data-name="Trazado 1" d="M0,151.022l62.719,12.6,38.728-12.793-2.061-2.1L63.108,157.71.35,148.3Z" transform="translate(0 -90.635)" fill="#fff"/>
+      <path id="Trazado_2" data-name="Trazado 2" d="M7.8,125.1l9.682.078.117,7.232-9.643-1.128Z" transform="translate(-4.767 -76.456)" fill="#fff"/>
+      <path id="Trazado_3" data-name="Trazado 3" d="M39.8,123.705l14.931-.505-.078,10.46-14.659-1.633Z" transform="translate(-24.324 -75.295)" fill="#fff"/>
+      <path id="Trazado_4" data-name="Trazado 4" d="M40.1,92.833,55.109,89.8l.078,11-15.009-.078Z" transform="translate(-24.508 -54.882)" fill="#fff"/>
+      <path id="Trazado_5" data-name="Trazado 5" d="M87.7,121.167,116.28,120l-.272,14.815-27.919-2.8Z" transform="translate(-53.599 -73.339)" fill="#fff"/>
+      <path id="Trazado_6" data-name="Trazado 6" d="M87.978,78.488,116.285,73.2l.272,14.931L87.9,89.142Z" transform="translate(-53.721 -44.737)" fill="#fff"/>
+      <path id="Trazado_7" data-name="Trazado 7" d="M8.494,101.661,18.06,100.3l.078,7.5L8.3,108Z" transform="translate(-5.073 -61.3)" fill="#fff"/>
+      <path id="Trazado_8" data-name="Trazado 8" d="M8,76.511,17.565,74.1l.272,7.271L8,82.888Z" transform="translate(-4.889 -45.287)" fill="#fff"/>
+      <path id="Trazado_9" data-name="Trazado 9" d="M40.317,61.494,54.976,56.4l.272,10.071L40.2,69.776Z" transform="translate(-24.569 -34.47)" fill="#fff"/>
+      <path id="Trazado_10" data-name="Trazado 10" d="M87.672,35.243,116.1,24.9l.389,15.165L87.4,46.053Z" transform="translate(-53.416 -15.218)" fill="#fff"/>
+      <path id="Trazado_11" data-name="Trazado 11" d="M168.4,72.5l12.832,3.811-.311,12.365-12.248-.894Z" transform="translate(-102.92 -44.309)" fill="#fff"/>
+      <path id="Trazado_12" data-name="Trazado 12" d="M206.5,83.3l7.777,1.944.272,11.354-7.777-.778Z" transform="translate(-126.205 -50.91)" fill="#fff"/>
+      <g id="Grupo_2" data-name="Grupo 2">
+        <g id="Grupo_1" data-name="Grupo 1" clip-path="url(#clip-path)">
+          <path id="Trazado_13" data-name="Trazado 13" d="M225.8,60.8l7.427,3.344L233.11,73.4l-7.038-1.983C225.956,64.805,226.072,71.454,225.8,60.8Z" transform="translate(-138.001 -37.159)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.5"/>
+          <path id="Trazado_14" data-name="Trazado 14" d="M244.64,69.4l6.1,2.8.156,8.049-6.416-1.594C244.445,72.627,244.523,78.693,244.64,69.4Z" transform="translate(-149.413 -42.415)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.5"/>
+          <path id="Trazado_15" data-name="Trazado 15" d="M244.872,93.5l6.1,1.516v4.627l-.039,4.977-6.338.505C244.872,93.733,244.639,102.093,244.872,93.5Z" transform="translate(-149.49 -57.144)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.5"/>
+          <path id="Trazado_16" data-name="Trazado 16" d="M168.556,119.5l12.171.894-.078,11.782L168.4,134.587Z" transform="translate(-102.92 -73.034)" fill="#fff"/>
+          <path id="Trazado_17" data-name="Trazado 17" d="M206.2,122.7l7.388.311.078,9.06-7.466,1.711Z" transform="translate(-126.022 -74.99)" fill="#fff"/>
+          <path id="Trazado_18" data-name="Trazado 18" d="M230.4,123.494l5.483-.194H235.8l.078,7.349c-5.094,1.439-3.888,1.089-5.094,1.439Z" transform="translate(-140.812 -75.356)" fill="#fff"/>
+          <path id="Trazado_19" data-name="Trazado 19" d="M244.217,122.505l6.416-.505.117,6.066c-5.21,1.322-5.366,1.361-6.649,1.672C244.1,122.389,244.217,124.955,244.217,122.505Z" transform="translate(-149.185 -74.562)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.5"/>
+          <path id="Trazado_20" data-name="Trazado 20" d="M10.1,64.132l9.1-2.644V56.9Z" transform="translate(-6.173 -34.775)" fill="#fff"/>
+          <path id="Trazado_21" data-name="Trazado 21" d="M39.3,38.51l8.438-7.31,6.766.039.039,6.494L39.533,44.3Z" transform="translate(-24.019 -19.068)" fill="#fff"/>
+          <path id="Trazado_22" data-name="Trazado 22" d="M87.9,21.327,115.546,17.4,87.978,27.9Z" transform="translate(-53.721 -10.634)" fill="#fff"/>
+          <path id="Trazado_23" data-name="Trazado 23" d="M167.678,0l5.172,3.888.622-.389-.194-1.011,4.2,3.111V7.31l4.083,3.266-.194,9.565L167.6,13.376Z" transform="translate(-102.431)" fill="#fff"/>
+          <path id="Trazado_24" data-name="Trazado 24" d="M204.772,66.326,203.1,51.9l8.865,3.5.272,10.654" transform="translate(-124.127 -31.719)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.5"/>
+        </g>
+      </g>
+    </svg>
+    <ul>
+      <a href="" class="link-navegation"><li>Nosotros</li></a>
+      <li class="services">Servicios</li>
+      <ul>
+        <a href=""><li>INMEX</li></a>
+        <a href=""><li>Servicio de Almacenamiento</li></a>
+        <a href=""><li>Operador logístico 3 y 4pl</li></a>
+      </ul>
+      <a href="" class="link-navegation"><li>Blog</li></a>
+      <a href="" class="link-navegation"><li>Contacto</li></a>
+    </ul>
+    <div class="social-media">
+      <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="38.14" height="37.909" viewBox="0 0 38.14 37.909">
+        <path id="Icon_awesome-facebook" data-name="Icon awesome-facebook" d="M38.7,19.632a19.07,19.07,0,1,0-22.05,18.839V25.145H11.808V19.632h4.844v-4.2c0-4.779,2.845-7.419,7.2-7.419a29.348,29.348,0,0,1,4.269.372v4.691H25.719a2.756,2.756,0,0,0-3.107,2.978v3.579H27.9l-.846,5.513H22.612V38.472A19.077,19.077,0,0,0,38.7,19.632Z" transform="translate(-0.563 -0.563)" fill="#fff"/>
+      </svg></a>
+      <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="38.142" height="38.142" viewBox="0 0 38.142 38.142">
+        <path id="Icon_awesome-linkedin" data-name="Icon awesome-linkedin" d="M35.417,2.25H2.716A2.736,2.736,0,0,0,0,5V37.642a2.736,2.736,0,0,0,2.716,2.75h32.7a2.743,2.743,0,0,0,2.724-2.75V5A2.743,2.743,0,0,0,35.417,2.25ZM11.528,34.943H5.874V16.74h5.662v18.2ZM8.7,14.254a3.278,3.278,0,1,1,3.278-3.278A3.279,3.279,0,0,1,8.7,14.254ZM32.718,34.943H27.065V26.088c0-2.111-.043-4.827-2.937-4.827-2.946,0-3.4,2.3-3.4,4.674v9.008H15.078V16.74H20.5v2.486h.077a5.954,5.954,0,0,1,5.355-2.937c5.721,0,6.785,3.772,6.785,8.676Z" transform="translate(0 -2.25)" fill="#fff"/>
+      </svg></a>
+      <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="38.14" height="30.977" viewBox="0 0 38.14 30.977">
+        <path id="Icon_awesome-twitter" data-name="Icon awesome-twitter" d="M34.22,11.1c.024.339.024.678.024,1.016,0,10.334-7.865,22.24-22.24,22.24A22.09,22.09,0,0,1,0,30.848a16.17,16.17,0,0,0,1.888.1,15.655,15.655,0,0,0,9.7-3.34,7.831,7.831,0,0,1-7.309-5.421,9.857,9.857,0,0,0,1.476.121,8.267,8.267,0,0,0,2.057-.266,7.818,7.818,0,0,1-6.268-7.672v-.1a7.872,7.872,0,0,0,3.533.992A7.828,7.828,0,0,1,2.662,4.809a22.219,22.219,0,0,0,16.118,8.18,8.824,8.824,0,0,1-.194-1.791A7.824,7.824,0,0,1,32.114,5.849a15.39,15.39,0,0,0,4.961-1.888,7.8,7.8,0,0,1-3.436,4.308,15.67,15.67,0,0,0,4.5-1.21A16.8,16.8,0,0,1,34.22,11.1Z" transform="translate(0 -3.381)" fill="#fff"/>
+      </svg></a>
+    </div>
+    
+  </nav>
+</header>
+

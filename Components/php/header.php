bb971888ef5add76da2a5a664d3c02e1ba0c1b58
Marketing Auxiliar <marketingauxiliar055@gmail.com>
2023-04-05 16:57:05 -0600

    Se terminó el menú y parte del inicio de la página web

--- Components/php/header.php
+++ Components/php/header.php
@@ -1,6 +1,6 @@
 <header>
   <nav>  
-    <svg id="Grupo_3" data-name="Grupo 3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="101.663" height="72.985" viewBox="0 0 101.663 72.985">
+    <a href="/" class="Desktop-Logo"><svg id="Grupo_3" data-name="Grupo 3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="101.663" height="72.985" viewBox="0 0 101.663 72.985">
       <defs>
         <clipPath id="clip-path">
           <rect id="Rectángulo_2" data-name="Rectángulo 2" width="101.663" height="72.985" fill="none"/>
@@ -34,7 +34,7 @@
           <path id="Trazado_24" data-name="Trazado 24" d="M204.772,66.326,203.1,51.9l8.865,3.5.272,10.654" transform="translate(-124.127 -31.719)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.5"/>
         </g>
       </g>
-    </svg>
+    </svg></a>
     <ul>
       <a href="" class="link-navegation"><li>Nosotros</li></a>
       <li class="services">Servicios</li>
@@ -42,6 +42,9 @@
         <a href=""><li>INMEX</li></a>
         <a href=""><li>Servicio de Almacenamiento</li></a>
         <a href=""><li>Operador logístico 3 y 4pl</li></a>
+        <a href=""><li>Flete Terrestre</li></a>
+        <a href=""><li>Paquetería</li></a>
+        <a href=""><li>Transporte aéreo Nacional</li></a>
       </ul>
       <a href="" class="link-navegation"><li>Blog</li></a>
       <a href="" class="link-navegation"><li>Contacto</li></a>
@@ -57,7 +60,117 @@
         <path id="Icon_awesome-twitter" data-name="Icon awesome-twitter" d="M34.22,11.1c.024.339.024.678.024,1.016,0,10.334-7.865,22.24-22.24,22.24A22.09,22.09,0,0,1,0,30.848a16.17,16.17,0,0,0,1.888.1,15.655,15.655,0,0,0,9.7-3.34,7.831,7.831,0,0,1-7.309-5.421,9.857,9.857,0,0,0,1.476.121,8.267,8.267,0,0,0,2.057-.266,7.818,7.818,0,0,1-6.268-7.672v-.1a7.872,7.872,0,0,0,3.533.992A7.828,7.828,0,0,1,2.662,4.809a22.219,22.219,0,0,0,16.118,8.18,8.824,8.824,0,0,1-.194-1.791A7.824,7.824,0,0,1,32.114,5.849a15.39,15.39,0,0,0,4.961-1.888,7.8,7.8,0,0,1-3.436,4.308,15.67,15.67,0,0,0,4.5-1.21A16.8,16.8,0,0,1,34.22,11.1Z" transform="translate(0 -3.381)" fill="#fff"/>
       </svg></a>
     </div>
-    
+    <a href="" class="movil-logo"><svg id="Group_45" data-name="Group 45" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="81.111" height="58.202" viewBox="0 0 81.111 58.202">
+      <defs>
+        <clipPath id="clip-path">
+          <rect id="Rectangle_71" data-name="Rectangle 71" width="81.111" height="58.202" fill="none"/>
+        </clipPath>
+      </defs>
+      <path id="Path_134" data-name="Path 134" d="M0,150.471l50.016,10.047,30.884-10.2-1.643-1.674L50.326,155.8.279,148.3Z" transform="translate(0 -102.315)" fill="#022e5b"/>
+      <path id="Path_135" data-name="Path 135" d="M7.8,125.1l7.721.062.093,5.767-7.69-.9Z" transform="translate(-5.381 -86.309)" fill="#022e5b"/>
+      <path id="Path_136" data-name="Path 136" d="M39.8,123.6l11.907-.4-.062,8.341-11.69-1.3Z" transform="translate(-27.459 -84.998)" fill="#022e5b"/>
+      <path id="Path_137" data-name="Path 137" d="M40.1,92.219,52.069,89.8l.062,8.775-11.969-.062Z" transform="translate(-27.666 -61.955)" fill="#022e5b"/>
+      <path id="Path_138" data-name="Path 138" d="M87.7,120.93l22.791-.93-.217,11.814L88.01,129.581Z" transform="translate(-60.506 -82.79)" fill="#022e5b"/>
+      <path id="Path_139" data-name="Path 139" d="M87.962,77.417,110.536,73.2l.217,11.907L87.9,85.913Z" transform="translate(-60.644 -50.502)" fill="#022e5b"/>
+      <path id="Path_140" data-name="Path 140" d="M8.455,101.385l7.628-1.085.062,5.985L8.3,106.44Z" transform="translate(-5.726 -69.199)" fill="#022e5b"/>
+      <path id="Path_141" data-name="Path 141" d="M8,76.022,15.628,74.1l.217,5.8L8,81.108Z" transform="translate(-5.519 -51.123)" fill="#022e5b"/>
+      <path id="Path_142" data-name="Path 142" d="M40.293,60.462,51.983,56.4l.217,8.031-12,2.636Z" transform="translate(-27.735 -38.912)" fill="#022e5b"/>
+      <path id="Path_143" data-name="Path 143" d="M87.617,33.148,110.284,24.9l.31,12.093L87.4,41.768Z" transform="translate(-60.299 -17.179)" fill="#022e5b"/>
+      <path id="Path_144" data-name="Path 144" d="M168.4,72.5l10.233,3.039-.248,9.861-9.768-.713Z" transform="translate(-116.183 -50.019)" fill="#022e5b"/>
+      <path id="Path_145" data-name="Path 145" d="M206.5,83.3l6.2,1.55.217,9.054-6.2-.62Z" transform="translate(-142.469 -57.47)" fill="#022e5b"/>
+      <g id="Group_44" data-name="Group 44">
+        <g id="Group_43" data-name="Group 43" clip-path="url(#clip-path)">
+          <path id="Path_146" data-name="Path 146" d="M225.8,60.8l5.923,2.667-.093,7.38-5.612-1.581C225.924,63.994,226.017,69.3,225.8,60.8Z" transform="translate(-155.784 -41.947)" fill="none" stroke="#022e5b" stroke-width="0.788"/>
+          <path id="Path_147" data-name="Path 147" d="M244.606,69.4l4.868,2.233.124,6.419-5.116-1.271C244.451,71.974,244.513,76.811,244.606,69.4Z" transform="translate(-168.668 -47.88)" fill="none" stroke="#022e5b" stroke-width="0.72"/>
+          <path id="Path_148" data-name="Path 148" d="M244.817,93.5l4.868,1.209V98.4l-.031,3.969-5.054.4C244.817,93.686,244.631,100.353,244.817,93.5Z" transform="translate(-168.755 -64.508)" fill="none" stroke="#022e5b" stroke-width="0.72"/>
+          <path id="Path_149" data-name="Path 149" d="M168.524,119.5l9.705.713-.062,9.4-9.768,1.923Z" transform="translate(-116.183 -82.445)" fill="#022e5b"/>
+          <path id="Path_150" data-name="Path 150" d="M206.2,122.7l5.892.248.062,7.225-5.954,1.364Z" transform="translate(-142.262 -84.653)" fill="#022e5b"/>
+          <path id="Path_151" data-name="Path 151" d="M230.4,123.455l4.372-.155h-.062l.062,5.861c-4.062,1.147-3.1.868-4.062,1.147Z" transform="translate(-158.958 -85.067)" fill="#022e5b"/>
+          <path id="Path_152" data-name="Path 152" d="M244.193,122.4l5.116-.4.093,4.837c-4.155,1.054-4.279,1.085-5.3,1.333C244.1,122.31,244.193,124.357,244.193,122.4Z" transform="translate(-168.41 -84.17)" fill="none" stroke="#022e5b" stroke-width="0.753"/>
+          <path id="Path_153" data-name="Path 153" d="M10.1,62.667l7.256-2.109V56.9Z" transform="translate(-6.968 -39.256)" fill="#022e5b"/>
+          <path id="Path_154" data-name="Path 154" d="M39.3,37.03,46.029,31.2l5.4.031.031,5.178L39.486,41.65Z" transform="translate(-27.114 -21.526)" fill="#022e5b"/>
+          <path id="Path_155" data-name="Path 155" d="M87.9,20.532,109.947,17.4,87.962,25.772Z" transform="translate(-60.644 -12.005)" fill="#022e5b"/>
+          <path id="Path_156" data-name="Path 156" d="M167.662,0l4.124,3.1.5-.31-.155-.806,3.349,2.481V5.829l3.256,2.6-.155,7.628-10.977-5.4Z" transform="translate(-115.631)" fill="#022e5b"/>
+          <path id="Path_157" data-name="Path 157" d="M204.433,63.4,203.1,51.9l7.07,2.791.217,8.5" transform="translate(-140.123 -35.807)" fill="none" stroke="#022e5b" stroke-width="0.8"/>
+        </g>
+      </g>
+    </svg></a>
+    <label for="responsive-menu" class="active-responsive-menu">
+      <svg xmlns="http://www.w3.org/2000/svg" width="27.992" height="24.388" viewBox="0 0 27.992 24.388">
+        <path id="Icon_open-menu" data-name="Icon open-menu" d="M0,0V3.5H27.992V0ZM0,10.392v3.5H27.992v-3.5Zm0,10.5v3.5H27.992v-3.5Z" fill="#1c3156"/>
+      </svg>
+    </label>
+    <input type="checkbox" name="responsive-menu" id="responsive-menu">
+    <div class="Nav-responsive">
+      <label for="responsive-menu" class="close-responsive-menu"><svg xmlns="http://www.w3.org/2000/svg" width="17.036" height="17.036" viewBox="0 0 17.036 17.036">
+        <path id="Icon_ionic-ios-close-circle" data-name="Icon ionic-ios-close-circle" d="M11.893,3.375a8.518,8.518,0,1,0,8.518,8.518A8.517,8.517,0,0,0,11.893,3.375Zm2.158,11.6-2.158-2.158L9.735,14.977a.654.654,0,1,1-.926-.926l2.158-2.158L8.809,9.735a.654.654,0,0,1,.926-.926l2.158,2.158,2.158-2.158a.654.654,0,1,1,.926.926l-2.158,2.158,2.158,2.158a.658.658,0,0,1,0,.926A.65.65,0,0,1,14.051,14.977Z" transform="translate(-3.375 -3.375)" fill="#1c3156"/>
+      </svg></label>
+      <a href="/" class="logo-menu-movil"><svg id="Group_46" data-name="Group 46" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="113.976" height="81.784" viewBox="0 0 113.976 81.784">
+        <defs>
+          <clipPath id="clip-path">
+            <rect id="Rectangle_71" data-name="Rectangle 71" width="113.976" height="81.784" fill="none"/>
+          </clipPath>
+        </defs>
+        <path id="Path_134" data-name="Path 134" d="M0,151.35l70.281,14.117,43.4-14.335-2.309-2.353L70.717,158.844.392,148.3Z" transform="translate(0 -83.683)" fill="#022e5b"/>
+        <path id="Path_135" data-name="Path 135" d="M7.8,125.1l10.849.087.131,8.1L7.974,132.028Z" transform="translate(-4.401 -70.592)" fill="#022e5b"/>
+        <path id="Path_136" data-name="Path 136" d="M39.8,123.766l16.732-.566-.087,11.721-16.427-1.83Z" transform="translate(-22.458 -69.52)" fill="#022e5b"/>
+        <path id="Path_137" data-name="Path 137" d="M40.1,93.2l16.819-3.4.087,12.331-16.819-.087Z" transform="translate(-22.628 -50.673)" fill="#022e5b"/>
+        <path id="Path_138" data-name="Path 138" d="M87.7,121.307,119.725,120l-.3,16.6-31.284-3.137Z" transform="translate(-49.488 -67.714)" fill="#022e5b"/>
+        <path id="Path_139" data-name="Path 139" d="M87.987,79.126l31.72-5.926.3,16.732L87.9,91.064Z" transform="translate(-49.6 -41.305)" fill="#022e5b"/>
+        <path id="Path_140" data-name="Path 140" d="M8.518,101.825,19.237,100.3l.087,8.409L8.3,108.927Z" transform="translate(-4.684 -56.598)" fill="#022e5b"/>
+        <path id="Path_141" data-name="Path 141" d="M8,76.8l10.719-2.7.305,8.148L8,83.947Z" transform="translate(-4.514 -41.813)" fill="#022e5b"/>
+        <path id="Path_142" data-name="Path 142" d="M40.331,62.108,56.757,56.4l.305,11.285L40.2,71.389Z" transform="translate(-22.684 -31.826)" fill="#022e5b"/>
+        <path id="Path_143" data-name="Path 143" d="M87.7,36.49,119.556,24.9l.436,16.993L87.4,48.6Z" transform="translate(-49.318 -14.051)" fill="#022e5b"/>
+        <path id="Path_144" data-name="Path 144" d="M168.4,72.5l14.379,4.27-.349,13.856-13.725-1Z" transform="translate(-95.025 -40.911)" fill="#022e5b"/>
+        <path id="Path_145" data-name="Path 145" d="M206.5,83.3l8.714,2.179.3,12.723L206.8,97.33Z" transform="translate(-116.524 -47.005)" fill="#022e5b"/>
+        <g id="Group_44" data-name="Group 44">
+          <g id="Group_43" data-name="Group 43" clip-path="url(#clip-path)">
+            <path id="Path_146" data-name="Path 146" d="M225.8,60.8l8.322,3.747-.131,10.37L226.1,72.7C225.974,65.288,226.1,72.739,225.8,60.8Z" transform="translate(-127.415 -34.308)" fill="none" stroke="#022e5b" stroke-width="0.788"/>
+            <path id="Path_147" data-name="Path 147" d="M244.66,69.4l6.841,3.137.174,9.019-7.189-1.786C244.442,73.016,244.529,79.814,244.66,69.4Z" transform="translate(-137.952 -39.161)" fill="none" stroke="#022e5b" stroke-width="0.72"/>
+            <path id="Path_148" data-name="Path 148" d="M244.9,93.5l6.841,1.7v5.185l-.044,5.577-7.1.566C244.9,93.761,244.644,103.129,244.9,93.5Z" transform="translate(-138.024 -52.76)" fill="none" stroke="#022e5b" stroke-width="0.72"/>
+            <path id="Path_149" data-name="Path 149" d="M168.574,119.5l13.638,1-.087,13.2-13.725,2.7Z" transform="translate(-95.025 -67.432)" fill="#022e5b"/>
+            <path id="Path_150" data-name="Path 150" d="M206.2,122.7l8.279.349.087,10.152-8.366,1.917Z" transform="translate(-116.355 -69.237)" fill="#022e5b"/>
+            <path id="Path_151" data-name="Path 151" d="M230.4,123.518l6.144-.218h-.087l.087,8.235c-5.708,1.612-4.357,1.22-5.708,1.612Z" transform="translate(-130.011 -69.576)" fill="#022e5b"/>
+            <path id="Path_152" data-name="Path 152" d="M244.231,122.566,251.42,122l.131,6.8c-5.839,1.481-6.013,1.525-7.451,1.874C244.1,122.436,244.231,125.311,244.231,122.566Z" transform="translate(-137.741 -68.842)" fill="none" stroke="#022e5b" stroke-width="0.753"/>
+            <path id="Path_153" data-name="Path 153" d="M10.1,65l10.2-2.963V56.9Z" transform="translate(-5.699 -32.108)" fill="#022e5b"/>
+            <path id="Path_154" data-name="Path 154" d="M39.3,39.391,48.755,31.2l7.581.044.044,7.276L39.561,45.884Z" transform="translate(-22.176 -17.606)" fill="#022e5b"/>
+            <path id="Path_155" data-name="Path 155" d="M87.9,21.8l30.979-4.4L87.987,29.164Z" transform="translate(-49.6 -9.819)" fill="#022e5b"/>
+            <path id="Path_156" data-name="Path 156" d="M167.687,0l5.8,4.357.7-.436-.218-1.133,4.706,3.486V8.191l4.575,3.66-.218,10.719L167.6,14.989Z" transform="translate(-94.574)" fill="#022e5b"/>
+            <path id="Path_157" data-name="Path 157" d="M204.974,68.065,203.1,51.9l9.934,3.921.3,11.939" transform="translate(-114.606 -29.286)" fill="none" stroke="#022e5b" stroke-width="0.8"/>
+          </g>
+        </g>
+      </svg></a>
+      <ul class="responsive-nav-menu">
+        <a href=""><li>NOSOTROS</li></a>
+        <li class="mobile-dropdown-services">SERVICIOS 
+          <label for="Movile-Services"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 18 16">
+            <path id="Polygon_2" data-name="Polygon 2" d="M9,0l9,16H0Z" transform="translate(18 16) rotate(180)" fill="#1f3c90"/>
+          </svg></label>
+        </li>
+        <input type="checkbox" name="Movile-Services" id="Movile-Services">
+        <ul class="movilServices">
+          <a href=""><li>INMEX</li></a>
+          <a href=""><li>ALMACENAJE</li></a>
+          <a href=""><li>OPERADOR LOGÍSTICO 3 Y 4PL</li></a>
+          <a href=""><li>FLETE TERRESTRE</li></a>
+          <a href=""><li>PAQUETERÍA</li></a>
+          <a href=""><li>TRANSPORTE AÉREO INTERNACIONAL</li></a>
+        </ul>
+        <a href=""><li>BLOG</li></a>
+        <a href=""><li>CONTACTO</li></a>
+      </ul>
+      <div class="social-media-movil">
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="38.14" height="37.909" viewBox="0 0 38.14 37.909">
+          <path id="Icon_awesome-facebook" data-name="Icon awesome-facebook" d="M38.7,19.632a19.07,19.07,0,1,0-22.05,18.839V25.145H11.808V19.632h4.844v-4.2c0-4.779,2.845-7.419,7.2-7.419a29.348,29.348,0,0,1,4.269.372v4.691H25.719a2.756,2.756,0,0,0-3.107,2.978v3.579H27.9l-.846,5.513H22.612V38.472A19.077,19.077,0,0,0,38.7,19.632Z" transform="translate(-0.563 -0.563)" fill="#1F3C90"/>
+        </svg></a>
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="38.142" height="38.142" viewBox="0 0 38.142 38.142">
+          <path id="Icon_awesome-linkedin" data-name="Icon awesome-linkedin" d="M35.417,2.25H2.716A2.736,2.736,0,0,0,0,5V37.642a2.736,2.736,0,0,0,2.716,2.75h32.7a2.743,2.743,0,0,0,2.724-2.75V5A2.743,2.743,0,0,0,35.417,2.25ZM11.528,34.943H5.874V16.74h5.662v18.2ZM8.7,14.254a3.278,3.278,0,1,1,3.278-3.278A3.279,3.279,0,0,1,8.7,14.254ZM32.718,34.943H27.065V26.088c0-2.111-.043-4.827-2.937-4.827-2.946,0-3.4,2.3-3.4,4.674v9.008H15.078V16.74H20.5v2.486h.077a5.954,5.954,0,0,1,5.355-2.937c5.721,0,6.785,3.772,6.785,8.676Z" transform="translate(0 -2.25)" fill="#1F3C90"/>
+        </svg></a>
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="38.14" height="30.977" viewBox="0 0 38.14 30.977">
+          <path id="Icon_awesome-twitter" data-name="Icon awesome-twitter" d="M34.22,11.1c.024.339.024.678.024,1.016,0,10.334-7.865,22.24-22.24,22.24A22.09,22.09,0,0,1,0,30.848a16.17,16.17,0,0,0,1.888.1,15.655,15.655,0,0,0,9.7-3.34,7.831,7.831,0,0,1-7.309-5.421,9.857,9.857,0,0,0,1.476.121,8.267,8.267,0,0,0,2.057-.266,7.818,7.818,0,0,1-6.268-7.672v-.1a7.872,7.872,0,0,0,3.533.992A7.828,7.828,0,0,1,2.662,4.809a22.219,22.219,0,0,0,16.118,8.18,8.824,8.824,0,0,1-.194-1.791A7.824,7.824,0,0,1,32.114,5.849a15.39,15.39,0,0,0,4.961-1.888,7.8,7.8,0,0,1-3.436,4.308,15.67,15.67,0,0,0,4.5-1.21A16.8,16.8,0,0,1,34.22,11.1Z" transform="translate(0 -3.381)" fill="#1F3C90"/>
+        </svg></a>
+      </div>
+    </div>
   </nav>
 </header>
 
--- index.php
+++ index.php
@@ -10,9 +10,23 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/png" href="/favicon.ico">
 </head>
 <body>
   <?php include ('Components/php/header.php') ?> 
-  
+    <main>
+      <section class="Cover">
+        <img src="img/Almacén_coincap.jpg" alt="Almacen en la Ciudad de México, Consorcio Internacional Caporali">
+        <div class="Cover--Information">
+          <p>Te ofrecemos un sin fin de </p>
+          <h1>SOLUCIONES LOGÍSTICAS EN PARA TU EMPRESA EN MÉXICO </h1>
+          <p>Somos tu operador logístico los 365 días del año con metodologías personalizadas para tu negocio. ¡Entra y descubre más!</p>
+          <div>
+            <a href=""><button>COTIZAR YA</button></a>
+            <a href=""><button>SERVICIOS</button></a>
+          </div>
+        </div>
+      </section>
+    </main>
 </body>
 </html>

5bea65df5d85a18f2863a4261367de2a185dddea
Atzahel <armandoatzahell@gmail.com>
2023-04-11 10:51:04 -0600

    Se avanzó al inicio agregando la segunda sección con estilos css y html

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-  <link rel="icon" type="image/png" href="/favicon.ico">
+  <link rel="icon" type="image/png" href="favicon.ico">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
   <?php include ('Components/php/header.php') ?> 
@@ -27,6 +28,60 @@
           </div>
         </div>
       </section>
+      <section class="Consorcio-Overview">
+        <h2>Consorcio Internacional Caporali</h2>
+        <p>Tu aliado logístico</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, set. </p>
+        <div class="benefits">
+          <div class="benefits__item">
+            <img src="svg/truck1.svg" alt="Camioneta de carga">
+            <h3>Soluciones logísticas</h3>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+          </div>
+          <div class="benefits__item">
+            <img src="svg/montacargas.svg" alt="Montacargas">
+            <h3>Gestión de mercancía</h3>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+          </div>
+          <div class="benefits__item">
+            <img src="svg/truck2.svg" alt="Camión de carga">
+            <h3>Personal Capacitado</h3>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+          </div>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+      </section>
+      <section class="services">
+        <div class="services--first">
+          <h2>NUESTROS <br> SERVICIOS</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad distinctio asperiores odit illo odio est fugit exercitationem dolorum porro assumenda, cupiditate quis laudantium obcaecati eligendi possimus! Dolorum molestiae inventore voluptates.</p>
+        </div>
+        <div class="services--cards">
+          <div class="services--cards__item">
+            <img src="/img/Camion Inter 2011.jpg" alt="Camión Inter 2011, Consorcio Internacional Caporali" title="Servicio de Flete terrestre, Consorcio Internacional Caporali">
+            <h3>FLETE TERRESTRE</h3>
+            <a href=""><button>VER MÁS</button></a>
+          </div>
+          <div class="services--cards__item">
+            <img src="img/Almacen Ciudad de Mexico.jpg" alt="Almacen ciudad de México, Consorcio Internacional Caporali">
+            <h3>ALMACENAJE</h3>
+            <a href=""><button>VER MÁS</button></a>
+          </div>
+          <div class="services--cards__item">
+            <img src="/img/RAM 2017, Clase C2.jpg" alt="Servicio Inmex Consorcio Internacional Caporali" title="Servicio Inmex Consorcio Internacional Caporali">
+            <h3>INMEX</h3>
+            <a href=""><button>VER MÁS</button></a>
+          </div>
+        </div>
+        <div class="services--thrid">
+          <div>
+            <h2>Tu aliado logístico</h2>
+            <p>Somos tu operador logístico los 365 días del año con metodologías personalizadas para tu negocio. ¡Entra y descubre más!</p>
+          </div>
+          <a href=""><button>VER MÁS SERVICIOS</button></a>
+        </div>
+      </section>
+      <section></section>
     </main>
 </body>
 </html>

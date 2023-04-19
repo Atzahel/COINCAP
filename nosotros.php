bf92402d4c75c6c152ace4b5fcbca3ba6894ab2f
Marketing Auxiliar <marketingauxiliar055@gmail.com>
2023-04-19 16:05:05 -0600

    Se agregó la página acerca de nosotros al entorno, agregando Schema a las imágenes y breadcumb

---
+++ nosotros.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Acerca de nosotros | Consorcio Internacional Caporali</title>
+  <link rel="stylesheet" href="css/nosotros.css">
+  <!-- Link Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/png" href="favicon.ico">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+  <?php include ('Components/php/header.php') ?> 
+  <section class="cover">
+    <img src="img/Camion-carga-gran-volumen.jpg" alt="Camión de carga de gran volumen">
+    <ul class="breadcumb">
+      <li><a href="">Inicio | </a></li>
+      <li><a href="">nosotros</a></li>
+    </ul>
+  </section>
+  <section class="about">
+    <img src="svg/icon_coincap.svg" alt="Consorcio Internacional Caporali logo svg">
+    <div class="about--information">
+      <h1>Conoce nuestra empresa </h1>
+      <p> Tenemos un enfoque en buscar soluciones a requerimientos específicos para solucionar el desarrollo logístico de inicio a fin.</p>
+      <p>Contamos con los mejores profesionales; los cuales tienen como objetivo principal generar soluciones, mantener la confiabilidad, certeza y servicio a nuestros clientes sin dejar de lado el expertiz para poder realizarlo en el menor tiempo posible.</p>
+    </div>
+  </section>
+  <section class="wearehouse">
+    <h2>Ten tu producto mucho más cerca de tu cliente utilizando nuestros almacenes</h2>
+    <div class="wearehouse--container">
+      <img src="img/Almacen_CDMX.jpg" alt="Almacén logístico Ciudad de México">
+      <div class="wearehouse--container__item">
+        <h3>Almacén en Ciudad de México</h3>
+        <p>Nuestro almacén, ubicado estratégicamente en la capital de la Ciudad de México, se encuentra diseñado para brindarte soluciones de almacenamiento y distribución de mercancía confiables y eficientes. Contamos con un área de más de 1500 m2 y Rack, que nos permite almacenar grandes volúmenes de productos.</p>
+        <p>Además, disponemos de 75m2 de espacio para servicios de SHUTTING y otros 50m2 con una altura máxima de 6 metros, techos y canalones de fibra de vidrio, y una rigurosa seguridad las 24 horas del día, los 7 días de la semana. También contamos con una puerta de acceso vehicular equipada con CCTV para garantizar la máxima seguridad y tranquilidad de nuestros clientes.</p>
+        <p>Nuestras instalaciones gozan de una ubicación privilegiada, situadas a tan solo 10 minutos del Aeropuerto Internacional de la Ciudad de México (AICM) y la aduana, lo que nos permite garantizar una logística ágil y eficiente para nuestros clientes.</p>
+      </div>
+    </div>
+    <div class="wearehouse--container">
+      <img src="img/Almacen_Tijuana.jpg" alt="Almacén logístico Tijuana">
+      <div class="wearehouse--container__item">
+        <h3>Almacén en la Ciudad de Tijuana</h3>
+        <p>Ofrecemos soluciones de almacenamiento y distribución de alta calidad en nuestro almacén en el centro de la ciudad. Con 600m2 de área de almacenaje, 100m2 de CROSSDOCKING, y otros 100m2 de SHUTTING y servicios, tus productos estarán en las mejores manos.</p>
+        <p>Nuestras instalaciones cuentan con techos y canalones aislados con fibra de vidrio y bajantes de 6, brindando una protección óptima. Además, tenemos seguridad 24/7 y una excelente ubicación a solo 5 minutos de la frontera con Estados Unidos.</p>
+        <p>Confía en nuestro servicio profesional y dinámico para el manejo seguro y eficiente de tu mercancía.</p>
+      </div>
+    </div>
+  </section>
+  <section class="trucks">
+    <h2>NUESTRAS UNIDADES</h2>
+    <div class="trucks--Cards-container">
+        <div class="trucks--Cards-container__item">
+          <img src="img/Camion Inter 2011.jpg" alt="Camion Inter 2011, carga de gran volúmen">
+          <div>
+            <h3>Camión Inter 2011 <br> Clase C3, 3 Ejes</h3>
+            <p>9.35m X 2.50m X 2.50m</p>
+          </div>
+        </div>
+        <div class="trucks--Cards-container__item">
+          <img src="img/RAM 2017, Clase C2.jpg" alt="Unidad RAM 2017, transporte de carga">
+          <div>
+            <h3>RAM 2017 <br> Clase C2, 2 Ejes</h3>
+            <p>3.15m X 2.33m X 2.43m</p>
+          </div>
+        </div>
+        <div class="trucks--Cards-container__item">
+          <img src="img/unidad-hiunday.jpg" alt="Unidad logística Hiunday">
+          <div>
+          <h3>Hiunday 2017 <br> Clase C2, 2 Ejes</h3>
+            <p>2.40m X 1.26m X 1.20m</p>
+          </div>
+        </div>
+        <div class="trucks--Cards-container__item">
+          <img src="" alt="">
+          <div>
+            <h3>Nissan 2013 <br> Clase C2, 2 Ejes</h3>
+            <p>2.63m X 1.58m X 1.69m</p>
+          </div>
+        </div>
+        <div class="trucks--Cards-container__item">
+          <img src="" alt="">
+          <div>
+            <h3>Nissan 2013 <br> Clase C2, 2 Ejes</h3>
+            <p>2.63m X 1.58m X 1.69m</p>
+          </div>
+        </div>
+    </div>
+  </section>
+  <?php include 'Components/php/footer.php'?>
+  
+<!-- ====================SCHEMA ============================= -->
+
+  <script type="application/ld+json">
+    // SCHEMA BREADCUMB
+    {
+      "@context": "http://schema.org/",
+      "@type": "BreadcrumbList",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "name": "Inicio",
+          "item": "https://www.coincap.com.mx"
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "name": "Nosotros",
+          "item": "https://www.coincap.com.mx/nosotros"
+        }
+      ]
+    }
+    // SCHEMA IMAGES
+    
+    <script type="application/ld+json">
+    [{
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://www.coincap.com.mx/img/Camion-carga-gran-volumen.jpg",
+      "license": "https://www.coincap.com.mx",
+      "acquireLicensePage": "https://www.coincap.com.mx/contacto",
+      "creditText": "Consorcio Internacional Caporali S.A. de C.V.",
+      "creator": {
+        "@type": "Organization",
+        "name": "Consorcio Internacional Caporali S.A. de C.V."
+       },
+      "copyrightNotice": "Consorcio Internacional Caporali S.A. de C.V."
+    },
+   {
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://www.coincap.com.mx/img/Almacen_CDMX.jpg",
+      "license": "https://www.coincap.com.mx",
+      "acquireLicensePage": "https://www.coincap.com.mx/contacto",
+      "creditText": "Consorcio Internacional Caporali",
+      "creator": {
+        "@type": "Organization",
+        "name": "Consorcio Internacional Caporali S.A. de C.V."
+       },
+      "copyrightNotice": "Consorcio Internacional Caporali S.A. de C.V."
+    },
+    {
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://www.coincap.com.mx/img/Almacen_Tijuana.jpg",
+      "license": "https://www.coincap.com.mx",
+      "acquireLicensePage": "https://www.coincap.com.mx/contacto",
+      "creditText": "Consorcio Internacional Caporali",
+      "creator": {
+        "@type": "Organization",
+        "name": "Consorcio Internacional Caporali S.A. de C.V."
+       },
+      "copyrightNotice": "Consorcio Internacional Caporali S.A. de C.V."
+    },
+    {
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://www.coincap.com.mx/img/Camion Inter 2011.jpg",
+      "license": "https://www.coincap.com.mx",
+      "acquireLicensePage": "https://www.coincap.com.mx/contacto",
+      "creditText": "Consorcio Internacional Caporali",
+      "creator": {
+        "@type": "Organization",
+        "name": "Consorcio Internacional Caporali S.A. de C.V."
+       },
+      "copyrightNotice": "Consorcio Internacional Caporali S.A. de C.V."
+    }, 
+    {
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://www.coincap.com.mx/img/RAM 2017, Clase C2.jpg",
+      "license": "https://www.coincap.com.mx",
+      "acquireLicensePage": "https://www.coincap.com.mx/contacto",
+      "creditText": "Consorcio Internacional Caporali",
+      "creator": {
+        "@type": "Organization",
+        "name": "Consorcio Internacional Caporali S.A. de C.V."
+       },
+      "copyrightNotice": "Consorcio Internacional Caporali S.A. de C.V."
+    },
+    {
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://www.coincap.com.mx/img/unidad-hiunday.jpg",
+      "license": "https://www.coincap.com.mx",
+      "acquireLicensePage": "https://www.coincap.com.mx/contacto",
+      "creditText": "Consorcio Internacional Caporali",
+      "creator": {
+        "@type": "Organization",
+        "name": "Consorcio Internacional Caporali S.A. de C.V."
+       },
+      "copyrightNotice": "Consorcio Internacional Caporali S.A. de C.V."
+    }]
+  </script>
+</body>
+</html>

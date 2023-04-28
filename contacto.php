366c2a5fae5fa763442dff09fda1174ce7306aa4
Atzahel López <armandoatzahell@gmail.com>
2023-04-28 13:22:04 -0600

    Página de contacto terminada con los estilos correspondientes, solo falta configurar el backend con php para los formularios y la página de contacto

---
+++ contacto.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Servicio de transporte aéreo en México</title>
+  <link rel="stylesheet" href="css/contacto.css">
+  <!-- Link Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/png" href="favicon.ico">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+  <?php include ('Components/php/header.php') ?> 
+    <section class="cover">
+        <img src="img/Fachada Almacen CDMX.jpg" alt="Almacén logístico en ciudad de méxico">
+        <ul class="breadcumb">
+        <li><a href="/">Inicio | </a></li>
+        <li><a href="#">Contacto</a></li>
+        </ul>
+    </section>
+    <section class="Information">
+        <h1>Contáctanos, te responderemos lo antes posible</h1>
+        <p>Nos preocupamos por brindarte la mejor atención rápida y eficiente para que podamos solucionar las problemáticas de tu negocio de cadena de suministro.</p>       
+    </section>
+    <section class="Call-us">
+        <div class="Call-us--items">
+            <svg xmlns="http://www.w3.org/2000/svg" width="68.405" height="97.656" viewBox="0 0 68.405 97.656">
+                <path id="Icon_material-location-on" data-name="Icon material-location-on" d="M41.7,3A34.166,34.166,0,0,0,7.5,37.18c0,25.635,34.2,63.476,34.2,63.476s34.2-37.842,34.2-63.476A34.166,34.166,0,0,0,41.7,3Zm0,46.387A12.207,12.207,0,1,1,53.918,37.18,12.216,12.216,0,0,1,41.7,49.387Z" transform="translate(-7.5 -3)" fill="#1c3156"/>
+            </svg>
+            <div>
+                <a href="https://goo.gl/maps/DEnVMR5KxzDPCr458" target="_blank" title="Ver en Google Maps"><h2>CDMX</h2></a>
+                <p>Norte #29  36° Col. Moctezuma, 2da Sección Alcaldía V. Carranza CP. 15530</p>
+            </div>
+        </div>
+        <div class="Call-us--items">
+            <svg xmlns="http://www.w3.org/2000/svg" width="68.405" height="97.656" viewBox="0 0 68.405 97.656">
+                <path id="Icon_material-location-on" data-name="Icon material-location-on" d="M41.7,3A34.166,34.166,0,0,0,7.5,37.18c0,25.635,34.2,63.476,34.2,63.476s34.2-37.842,34.2-63.476A34.166,34.166,0,0,0,41.7,3Zm0,46.387A12.207,12.207,0,1,1,53.918,37.18,12.216,12.216,0,0,1,41.7,49.387Z" transform="translate(-7.5 -3)" fill="#1c3156"/>
+            </svg>
+            <div>
+                <a href="https://goo.gl/maps/Zsuawu4YANevn7jH7" target="_blank" title="Ver en Google Maps"><h2>TIJUANA</h2></a>
+                <p>Parque Industrial CIBAC, Prolongación M. de Juárez #1089 Nave 7, Col. Linda Vista, Tijuana CP 22054</p>
+            </div>
+        </div>
+        <div class="Call-us--items">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+              <path id="Icon_material-call" data-name="Icon material-call" d="M24.611,47.778A84.159,84.159,0,0,0,61.222,84.389L73.444,72.167a5.524,5.524,0,0,1,5.667-1.333A63.372,63.372,0,0,0,98.944,74a5.572,5.572,0,0,1,5.556,5.556V98.944a5.572,5.572,0,0,1-5.556,5.556A94.436,94.436,0,0,1,4.5,10.056,5.572,5.572,0,0,1,10.056,4.5H29.5a5.572,5.572,0,0,1,5.556,5.556,63.113,63.113,0,0,0,3.167,19.833,5.575,5.575,0,0,1-1.389,5.667L24.611,47.778Z" transform="translate(-4.5 -4.5)" fill="#1c3156"/>
+            </svg>
+            <div>
+                <a href="tel:+5255623622386"><h2>Comunícate con nosotros</h2></a>
+                <p>+52 55623622386 ext. 106 o 107</p>
+            </div>
+        </div>
+        <div class="Call-us--items">
+            <svg xmlns="http://www.w3.org/2000/svg" width="125" height="100" viewBox="0 0 125 100">
+                <path id="Icon_material-email" data-name="Icon material-email" d="M115.5,6H15.5A12.483,12.483,0,0,0,3.063,18.5L3,93.5A12.537,12.537,0,0,0,15.5,106h100A12.537,12.537,0,0,0,128,93.5v-75A12.537,12.537,0,0,0,115.5,6Zm0,25-50,31.25L15.5,31V18.5l50,31.25,50-31.25Z" transform="translate(-3 -6)" fill="#1c3156"/>
+            </svg>
+            <div>
+                <a href="mailto:contacto@coincap.com.mx"><h2>Comunícate con nosotros</h2></a>
+                <p>contacto@coincap.com.mx</p>
+            </div>
+        </div>
+    </section>
+    <section class="form">
+        <h2>Déjanos un mensaje, te responderemos enseguida</h2>
+        <form action="">
+            <input type="text" name="" id="" placeholder="¿Cómo te llamas?">
+            <input type="email" name="" id="" placeholder="tuemail@example.com">
+            <input type="tel" name="" id="" placeholder="escribe tu teléfono">
+            <input type="text" name="" id="" placeholder="¿De qué negocio nos escribes?">
+            <textarea name="" id="" cols="30" rows="10" placeholder="¿En qué podemos ayudarte?"></textarea>
+            <input type="submit" value="Enviar">
+        </form>
+    </section>
+  <?php include 'Components/php/footer.php'?>
+  
+<!-- ====================SCHEMA ============================= -->
+
+  <script type="application/ld+json">
+    // SCHEMA BREADCUMB
+    [{
+      "@context": "http://schema.org/",
+      "@type": "BreadcrumbList",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "name": "Inicio",
+          "item": "https://www.coincap.com.mx"
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "name": "Contacto",
+          "item": "https://www.coincap.com.mx/contacto"
+        }
+      ]
+    },
+    // SCHEMA IMAGES
+    {
+      "@context": "https://schema.org/",
+      "@type": "ImageObject",
+      "contentUrl": "https://www.coincap.com.mx/img/",
+      "license": "https://www.coincap.com.mx",
+      "acquireLicensePage": "https://www.coincap.com.mx/contacto",
+      "creditText": "Consorcio Internacional Caporali S.A. de C.V.",
+      "creator": {
+        "@type": "Organization",
+        "name": "Consorcio Internacional Caporali S.A. de C.V."
+       },
+      "copyrightNotice": "Consorcio Internacional Caporali S.A. de C.V."
+    }]
+  </script>
+</body>
+</html>
